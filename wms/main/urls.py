226d9c540e69c01e4b9c19cecb428122c9ef92c5
Avish Jain <avishjain0@gmail.com>
2022-11-23 10:21:56 +0530

    admin part

--- wms/main/urls.py
+++ wms/main/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from main import views
 
 urlpatterns = [
-    path('', views.index, name="home"),
-    path('login/',views.login,name='login'),
-    path('register/',views.register,name='register')
+    path("", views.index, name="home"),
+    path("login/", views.login, name="login"),
+    path("register/", views.register, name="register"),
+    path("admin-main/", views.admin, name="admin"),
 ]
--- wms/main/views.py
+++ wms/main/views.py
@@ -1,21 +1,27 @@
-from django.shortcuts import render,HttpResponse, loader
+from django.shortcuts import render, HttpResponse, loader
+
 # from django.conf import settings
 # settings.configure()
 
 
 def index(request):
 
-    context={}
-    html_template = loader.get_template( 'landing.html' )
-
+    context = {}
+    html_template = loader.get_template("landing.html")
 
     return HttpResponse(html_template.render(context, request))
 
 
 def login(request):
-    return render(request,'login.html')
+    return render(request, "login.html")
 
 
 def register(request):
-    return render(request,'register.html')
+    return render(request, "register.html")
+
+
+def admin(request):
+    return render(request, "admin.html")
+
+
 # Create your views here.
