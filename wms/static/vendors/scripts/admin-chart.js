9991ef908b955ab04dc4537432749667d7ff01b5
Avish Jain <avishjain0@gmail.com>
2022-11-23 19:54:11 +0530

    admin changes

--- wms/static/vendors/scripts/admin-chart.js
+++ wms/static/vendors/scripts/admin-chart.js
@@ -14,11 +14,16 @@ var myChart = new Chart(ctx, {
       "Aug",
       "Sept",
       "Oct",
+      "Nov",
+      "Dec",
     ],
     datasets: [
       {
         label: "User 1", // Name the series
-        data: [4000, 5040, 2424, 14040, 8141, 5111, 3544, 4700, 6555, 7811], // Specify the data values array
+        data: [
+          4000, 5040, 2424, 14040, 8141, 5111, 3544, 4700, 6555, 7811, 9763,
+          2093,
+        ], // Specify the data values array
         fill: false,
         borderColor: "#2196f3", // Add custom color border (Line)
         backgroundColor: "#2196f3", // Add custom color background (Points and Fill)
@@ -27,7 +32,10 @@ var myChart = new Chart(ctx, {
       },
       {
         label: "User 2", // Name the series
-        data: [10880, 8942, 4545, 7588, 9900, 2482, 7417, 5504, 5900, 4570], // Specify the data values array
+        data: [
+          10880, 8942, 4545, 7588, 9900, 2482, 7417, 5504, 5900, 4570, 8753,
+          2380,
+        ], // Specify the data values array
         fill: false,
         borderColor: "#4CAF50", // Add custom color border (Line)
         backgroundColor: "#4CAF50", // Add custom color background (Points and Fill)
@@ -35,7 +43,10 @@ var myChart = new Chart(ctx, {
       },
       {
         label: "User 3", // Name the series
-        data: [10000, 7000, 1500, 4000, 3200, 7450, 5698, 7854, 6500, 7894], // Specify the data values array
+        data: [
+          10000, 7000, 1500, 4000, 3200, 7450, 5698, 7854, 6500, 7894, 4530,
+          8634,
+        ], // Specify the data values array
         fill: false,
         borderColor: "#000000", // Add custom color border (Line)
         backgroundColor: "#000000", // Add custom color background (Points and Fill)
